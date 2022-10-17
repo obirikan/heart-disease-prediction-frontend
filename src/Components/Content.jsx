d83c34c13285a82a71cd408a108378d62620c788
obirikan <obirikan020@gmail.com>
2022-10-17 00:16:58 +0000

    youtube posts

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import Ps from '../Components/assets/1.jpg'
+import P from '../Components/assets/6.JPG'
+import M from '../Components/assets/3.jpg'
 const Content = () => {
   return (
-    <div className='contentcomponent'>
+  <div className='contentcomponent'>
         <div>
           <div className='ctnheader'> 
-            <div className='child1'>image of user</div>
+            <div className='child1'><img src={P} alt="" className='ps1' /></div>
             <div className='child2'>
-              <p>name of person {''}<span>subscribed</span></p>
-              <p>time ago</p>
+              <p>Tonny {''}<span>subscribed</span></p>
+              <p className='ta'>1 hour ago</p>
             </div>
           </div>
-          <div className='text'><p>lorem30</p></div>
+          <div className='text'><p>RAISING CHAMPIONS EMPOWERING VISONS AND REACHING</p></div>
            <div>
-            <img src={Ps} alt="" className='psimg'/>
-           </div>
-           <div>
+            <img src={M} alt="" className='psimg'/>
+            <div>
              <div className='ctnbtn'>
                <p>icon1</p>
                <p>icon2</p>
              </div>
            </div>
+           </div>
         </div>
-    </div>
+  </div>
   )
 }
 
