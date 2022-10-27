1df82346282c7fda8a7dfebc7bd61736f32c10c3
obirikan <obirikan020@gmail.com>
2022-10-27 02:12:11 +0000

    first commit

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,29 +1,20 @@
 import React from 'react'
-import Ps from '../Components/assets/1.jpg'
-import P from '../Components/assets/6.JPG'
-import M from '../Components/assets/3.jpg'
+import d from '../assets/d.png'
+import { motion } from "framer-motion"
+import { Button } from '@chakra-ui/react'
+import { useNavigate } from "react-router-dom";
 const Content = () => {
+  const navigate=useNavigate()
   return (
-  <div className='contentcomponent'>
-        <div>
-          <div className='ctnheader'> 
-            <div className='child1'><img src={P} alt="" className='ps1' /></div>
-            <div className='child2'>
-              <p>Tonny {''}<span>subscribed</span></p>
-              <p className='ta'>1 hour ago</p>
-            </div>
-          </div>
-          <div className='text'><p>RAISING CHAMPIONS EMPOWERING VISONS AND REACHING</p></div>
-           <div>
-            <img src={M} alt="" className='psimg'/>
-            <div>
-             <div className='ctnbtn'>
-               <p>icon1</p>
-               <p>icon2</p>
-             </div>
-           </div>
-           </div>
-        </div>
+  <div  className='anim'>
+          <motion.img src={d} alt=""
+             animate={{ y: 90 }}
+             transition={{ type: "spring", stiffness: 50 ,delay:1}}
+          />
+          <h1>Heart Disease Predictor</h1>
+          <Button colorScheme='red' m={30} width={100} 
+          onClick={()=>navigate('/main')}
+          >Start</Button>
   </div>
   )
 }
